1ef7eac7477e55e1130f5141c8572845727de6b5
hongdi-zhao <41450583+hongdi-zhao@users.noreply.github.com>
2026-03-09 12:35:19 +0000

    Deploying to gh-pages from @ hongdi-zhao/hongdi-zhao.github.io@68d89998c7ac7ae7bd5ef5a3fb02283571312c6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "Dyson Graduate Teaching Assistant – Outstanding Service Award [2025]",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
